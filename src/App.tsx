3560cded0f46d91d1ae0c13125579c8c79afae98
omer_sm <smorodinskyomer@gmail.com>
2023-11-23 13:27:36 +0200

    did authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { authorizeUser, getToken } from './spotifyManager'
 
+let code: string | null
+window.onload = () => {
+  //if url has an auth code, get an access token
+  const urlParams = new URLSearchParams(window.location.search)
+  code = urlParams.get('code')
+  if (typeof code === "string") {
+    getToken(code).then(r => {
+      //TODO: notify user that login was successful
+    })
+  }
+  if (!code) {
+    console.log(urlParams.get('error'))
+  }
+}
 function App() {
+  //TODO dark mode on default and import font
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={authorizeUser}>click</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/spotifyManager.ts
@@ -0,0 +1,110 @@
+//do code challenge
+const generateRandomString = (length: number) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    const values = crypto.getRandomValues(new Uint8Array(length))
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "")
+}
+const sha256 = async (plain: string) => {
+    const encoder = new TextEncoder()
+    const data = encoder.encode(plain)
+    return window.crypto.subtle.digest('SHA-256', data)
+}
+const base64encode = (input: ArrayBuffer) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+        .replace(/=/g, '')
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+}
+
+const getCodeChallenge = async () => {
+    const codeVerifier = generateRandomString(64)
+    localStorage.setItem('code_verifier', codeVerifier)
+    const hashed = await sha256(codeVerifier)
+    const codeChallenge = base64encode(hashed)
+    return codeChallenge
+}
+
+//handle user authorization
+const clientID = "3423964c454a45518f97e5ade891b01e"
+const scope = 'user-read-private user-read-email'
+const authUrl = new URL("https://accounts.spotify.com/authorize")
+const redirectUri = "http://localhost:3000"
+const requestUserAuth = (codeChallenge: string) => {
+    const params = {
+        response_type: 'code',
+        client_id: clientID,
+        scope,
+        code_challenge_method: 'S256',
+        code_challenge: codeChallenge,
+        redirect_uri: redirectUri,
+    }
+    authUrl.search = new URLSearchParams(params).toString()
+    window.location.href = authUrl.toString()
+}
+export const authorizeUser = () => {
+    getCodeChallenge().then(r => {
+        requestUserAuth(r)
+    }).catch(e => {
+        console.log(e)
+    })
+}
+/**
+ * gets an access token when the user first logs in
+ * @param code auth code returned from login page
+ */
+export const getToken = async (code: string) => {
+    let codeVerifier = localStorage.getItem('code_verifier')
+    if (typeof codeVerifier === "string") {
+        const payload = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                client_id: clientID,
+                grant_type: 'authorization_code',
+                code,
+                redirect_uri: redirectUri,
+                code_verifier: codeVerifier,
+            }),
+        }
+        const body = await fetch("https://accounts.spotify.com/api/token", payload)
+        const response = await body.json()
+        localStorage.setItem('access_token', response.access_token)
+        localStorage.setItem('refresh_token', response.refresh_token)
+    } else {
+        console.error("Error: code verifier is undefined.")
+    }
+}
+/**
+ * refreshes the access token
+ */
+export const refreshToken = async () => {
+    const refreshToken = localStorage.getItem('refresh_token');
+    if (refreshToken){
+    const url = "https://accounts.spotify.com/api/token"
+     const payload = {
+       method: 'POST',
+       headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+       },
+       body: new URLSearchParams({
+         grant_type: 'refresh_token',
+         refresh_token: refreshToken,
+         client_id: clientID
+       }),
+     }
+     const body = await fetch(url, payload);
+     const response = await body.json();
+     localStorage.setItem('access_token', response.accessToken);
+     localStorage.setItem('refresh_token', response.refreshToken);
+    } else {
+        console.error("Error: cant refresh token as there was no refresh_token stored")
+    }
+   }
+
+
+
+
+
+
