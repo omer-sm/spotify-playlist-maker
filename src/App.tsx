d2ad9c7d37f53483620e7f40c614040c16d9e9a3
omer_sm <smorodinskyomer@gmail.com>
2023-11-24 07:05:22 +0200

    started making embed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { authorizeUser, getToken } from './spotifyManager'
+import PlayerEmbed from './Components/PlayerEmbed'
 
 let code: string | null
+let isUserLoggedIn = false
 window.onload = () => {
   //if url has an auth code, get an access token
   const urlParams = new URLSearchParams(window.location.search)
@@ -9,17 +11,31 @@ window.onload = () => {
   if (typeof code === "string") {
     getToken(code).then(r => {
       //TODO: notify user that login was successful
+      isUserLoggedIn = true
     })
   }
   if (!code) {
     console.log(urlParams.get('error'))
   }
 }
+//@ts-ignore
+window.onSpotifyIframeApiReady = (IFrameAPI) => {
+  const element = document.getElementById('embed-iframe');
+  const options = {
+      uri: 'spotify:episode:7makk4oTQel546B0PZlDM5'
+    };
+    //@ts-ignore
+  const callback = (EmbedController) => {};
+  IFrameAPI.createController(element, options, callback);
+}
+
 function App() {
   //TODO dark mode on default and import font
   return (
     <div className="App">
       <button onClick={authorizeUser}>click</button>
+      <PlayerEmbed />
+      
     </div>
   )
 }
---
+++ src/Components/PlayerEmbed.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+
+export default function PlayerEmbed() {
+    return (
+        <div id="embed-iframe"></div>
+    )
+}
